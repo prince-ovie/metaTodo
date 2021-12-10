031be52b297c6b11e1f43a757c8163f27ecb63b8
prince-ovie <dokuboprincewillovie@gmail.com>
2021-12-10 02:56:00 -0800

    god of war

---
+++ App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.heading}>MetaTodo GOD</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fafafa',
+    paddingTop: 64,
+    paddingHorizontal: 16,
+  },
+heading: {
+  fontSize: 32,
+  color: 'blue'
+}
+})
